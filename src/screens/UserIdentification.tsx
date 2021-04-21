a25ad4d6007b07fe5697d1ec425ebfa05cfde87f
Yslan Ramos <yslan3000@gmail.com>
2021-04-21 17:16:14 -0300

    add content frontend

--- src/screens/UserIdentification.tsx
+++ src/screens/UserIdentification.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button } from '../components/atoms/Button';
+import colors from '../styles/colors';
 import emojis from '../styles/emojis';
+import fonts from '../styles/fonts';
 
 export const UserIdentification: React.FC = () => {
   return (
@@ -8,6 +11,11 @@ export const UserIdentification: React.FC = () => {
       <View style={styles.content}>
         <View style={styles.form}>
           <Text style={styles.emoji}>{emojis.smiley}</Text>
+          <Text style={styles.title}>Como podemos {'\n'} chamar você?</Text>
+          <TextInput style={styles.input} placeholder='Digite seu nome' />
+          <View style={styles.footer}>
+            <Button title='Confirmar' />
+          </View>
         </View>
       </View>
     </SafeAreaView>
@@ -33,5 +41,28 @@ const styles = StyleSheet.create({
   },
   emoji: {
     fontSize: 44,
+  },
+  input: {
+    borderBottomWidth: 1,
+    borderColor: colors.gray,
+    color: colors.heading,
+    width: '100%',
+    fontSize: 18,
+    marginTop: 50,
+    padding: 10,
+    textAlign: 'center',
+  },
+  title: {
+    fontSize: 24,
+    textAlign: 'center',
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    lineHeight: 34,
+    marginTop: 20,
+  },
+  footer: {
+    marginTop: 40,
+    width: '100%',
+    paddingHorizontal: 20,
   },
 });
