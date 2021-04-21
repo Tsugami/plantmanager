ac3868e95acfb474e33755e2afc45f08d4fb96cf
Yslan Ramos <yslan3000@gmail.com>
2021-04-21 14:53:19 -0300

    run linter

--- App.tsx
+++ App.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import { Welcome } from './src/screens/Welcome';
 
 export default function App() {
-  return (
-    <Welcome />
-  )
+  return <Welcome />;
 }
 
 // import { StatusBar } from 'expo-status-bar';
