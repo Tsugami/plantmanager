1bdd33154ac55e50b7e7870fd2008814792d081f
Yslan Ramos <yslan3000@gmail.com>
2021-04-21 15:20:53 -0300

    chore: fix ts linter configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,18 @@
 module.exports = {
   root: true,
-  extends: ['@react-native-community', 'prettier', 'prettier/@typescript-eslint', 'prettier/react'],
+  extends: [
+    '@react-native-community',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
   parser: '@typescript-eslint/parser',
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+  },
 };
