a54afa95fe9e0d267fac978a846df838c010b3a7
Yslan Ramos <yslan3000@gmail.com>
2021-04-22 02:24:19 -0300

    add sort order desc

--- src/api/main/helpers/sort-items.ts
+++ src/api/main/helpers/sort-items.ts
@@ -1,5 +1,11 @@
 import { SortOrderTypes } from '../../domain/helpers/sort-param';
 
 export function sortItems<T>(key: keyof T, order: SortOrderTypes = 'asc'): (arr: T[]) => T[] {
-  return (arr) => arr.sort((a, b) => String(a[key]).localeCompare(String(b[key])));
+  return (arr) =>
+    arr.sort((a, b) => {
+      const main = order === 'asc' ? a[key] : b[key];
+      const compare = order === 'asc' ? b[key] : a[key];
+
+      return String(main).localeCompare(String(compare));
+    });
 }
